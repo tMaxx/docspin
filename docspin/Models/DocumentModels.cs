74e84ee713d27e02a179a9cfef584e0512b7ca32
dervill <idervill@gmail.com>
2015-05-06 15:32:29 +0200

    Documents

---
+++ docspin/Models/DocumentModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace docspin.Models
+{
+    public class DocumentContext : DbContext
+    {
+        public DocumentContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public System.Data.Entity.DbSet<docspin.Models.Document> Documents { get; set; }
+    }
+
+
+}
