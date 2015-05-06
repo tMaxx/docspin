15914397962ee55564edbe90ecb95c2ba3af7ad5
dervill <idervill@gmail.com>
2015-05-06 12:35:45 +0200

    Repo List

--- docspin/Controllers/RepositoryController.cs
+++ docspin/Controllers/RepositoryController.cs
@@ -1,34 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
-using System.Transactions;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
-using WebMatrix.WebData;
-using docspin.Filters;
 using docspin.Models;
 
 namespace docspin.Controllers
 {
     public class RepositoryController : Controller
     {
-        DataModelContainer db = new DataModelContainer();
+        private RepositoryContext db = new RepositoryContext();
 
-        //
         // GET: /Repository/
         public ActionResult Index()
         {
-            return View(db.RepositorySet.ToList());
+            return View(db.Repositories.ToList());
         }
 
-        // GET
+        // GET: /Repository/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Repository repository = db.Repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // GET: /Repository/Create
         public ActionResult Create()
         {
             return View();
         }
 
-	}
+        // POST: /Repository/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Name,ACS")] Repository repository)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Repositories.Add(repository);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(repository);
+        }
+
+        // GET: /Repository/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Repository repository = db.Repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // POST: /Repository/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Name,ACS")] Repository repository)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(repository).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(repository);
+        }
+
+        // GET: /Repository/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Repository repository = db.Repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // POST: /Repository/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Repository repository = db.Repositories.Find(id);
+            db.Repositories.Remove(repository);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
